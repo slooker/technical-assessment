8cba1b4d5a8a8e3181811a7d617502e6b265eec3
slooker <shawn.looker@gmail.com>
2017-12-15 11:55:24 -0500

    Added minor comments in routes.js

--- routes.js
+++ routes.js
@@ -2,24 +2,28 @@ const User = require('./controllers/user')
 const Article = require('./controllers/article')
 
 module.exports = (app) => {
+  // Create user
   app.post('/user', (req, res, next) => {
     return User.create(req.body)
     .then(data => res.json(data))
     .catch(next)
   })
 
+  // Create article
   app.post('/article', (req, res, next) => {
     return Article.create(req.body)
     .then(data => res.json(data))
     .catch(next)
   })
 
+  // Edit article
   app.put('/article/:id', (req, res, next) => {
     return Article.update(req.params.id, req.body)
     .then(data => res.json(data))
     .catch(next)
   })
 
+  // Get article by id
   app.get('/article/:id', (req, res, next) => {
     return Article.findById(req.params.id)
     .then(data => {
@@ -32,12 +36,14 @@ module.exports = (app) => {
     .catch(next)
   })
 
+  // Delete article
   app.delete('/article/:id', (req, res, next) => {
     Article.delete(req.params.id)
     .then(() => res.json({ deleted: req.params.id }))
     .catch(next)
   })
 
+  // Get list of articles by tag
   app.get('/articles/tag/:tag', (req, res, next) => {
     Article.findTag(req.params.tag)
     .then(data => res.json(data))
